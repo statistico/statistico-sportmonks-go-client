c1ebe39526a94e44a3e906ca03f49db55c6a6e4d
Joe Sweeny <joe.sweeny@opia-sp.com>
2018-12-18 22:17:01 +0000

    WIP: Client

--- client.go
+++ client.go
@@ -3,11 +3,21 @@ package sportmonks
 import (
 	"errors"
 	"net/http"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"strconv"
 )
 
-func NewClient(baseURL, apiKey string) (*Client, error) {
+type Client struct {
+	Client   *http.Client
+	BaseURL  string
+	ApiKey   string
+}
+
+func NewClient(baseURL string, apiKey string) (*Client, error) {
 	if baseURL == "" || apiKey == "" {
-		return nil, errors.New("Base URL and API Key are bothrequired to create a Client")
+		return &Client{}, errors.New("base URL and API Key are both required to create a Client")
 	}
 
 	return &Client{
@@ -19,4 +29,75 @@ func NewClient(baseURL, apiKey string) (*Client, error) {
 
 func (c *Client) SetHTTPClient(client *http.Client) {
 	c.Client = client
+}
+
+func (c *Client) Countries() (*CountryResponse, error) {
+	url := c.BaseURL + "/api/v2.0/countries?api_token=" + c.ApiKey
+
+	req, err := http.NewRequest("GET", url, nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := c.Client.Do(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+
+	r := new(ResponseMetaData)
+
+	err = json.Unmarshal([]byte(body), &r)
+
+	if err != nil {
+		return nil, errors.New("error when parsing request body")
+	}
+
+	countries := new(CountryResponse)
+
+	err = json.Unmarshal([]byte(body), &countries)
+
+	if err != nil {
+		return nil, errors.New("error when parsing request body")
+	}
+
+	for _, country := range countries.CountryList {
+		fmt.Printf("%+v\n", country)
+	}
+
+	if r.Meta.Pagination.Count > 0 {
+		page := r.Meta.Pagination.CurrentPage
+		total := r.Meta.Pagination.TotalPages
+
+		for i := page; i <= total; i++ {
+			fmt.Printf("%+v\n", url + "&Page=" + strconv.Itoa(i))
+
+			req, err := http.NewRequest("GET", url + "&page=" + strconv.Itoa(i), nil)
+
+			if err != nil {
+				return nil, err
+			}
+
+			res, err := c.Client.Do(req)
+
+			if err != nil {
+				return nil, err
+			}
+
+			body, err := ioutil.ReadAll(res.Body)
+
+			err = json.Unmarshal([]byte(body), &countries)
+
+			for _, country := range countries.CountryList {
+				fmt.Printf("%+v\n", country)
+			}
+		}
+	}
+
+	return countries, err
 }
--- types.go
+++ types.go
@@ -1,14 +1,42 @@
 package sportmonks
 
-import (
-	"net/http"
-)
-
 type (
-	// Client represents a SportMonks REST API Client
-	Client struct {
-		Client *http.Client
-		BaseURL string
-		ApiKey string
+	// Continent struct
+	Continent struct {
+		ID int `json:"id"`
+		Name string `json:"name"`
 	}
+
+	// Country struct
+	Country struct {
+		ID int `json:"id"`
+		Name string `json:"name"`
+		Data struct {
+			Continent string `json:"continent"`
+			SubRegion string `json:"sub_region"`
+			WorldRegion string `json:"world_region"`
+			Fifa string `json:"fifa"`
+			ISO string `json:"iso"`
+			Longitude string `json:"longitude"`
+			Latitude string `json:"latitude"`
+		} `json:"extra"`
+	}
+
+	CountryResponse struct {
+		CountryList []Country `json:"data"`
+	}
+
+	ResponseMetaData struct {
+		Meta struct {
+			Pagination struct {
+				Total int `json:"total"`
+				Count int `json:"count"`
+				PerPage int `json:"per_page"`
+				CurrentPage int `json:"current_page"`
+				TotalPages int `json:"total_pages"`
+			} `json:"pagination"`
+		} `json:"meta"`
+	}
+
+
 )
