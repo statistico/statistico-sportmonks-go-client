1884a3c847b6cc569195b3c917fc47e545782fc1
Joe Sweeny <joe.sweeny@opia-sp.com>
2019-01-24 20:46:12 +0000

    Remove unused constants

--- season.go
+++ season.go
@@ -1,8 +1,6 @@
 package sportmonks
 
 const seasonUri = "/api/v2.0/seasons"
-const IncludeFixtures = "fixtures"
-const IncludeResults = "results"
 
 func (c *Client) Seasons(page int, includes []string) (*SeasonsResponse, error) {
 	url := c.BaseURL + seasonUri + "?api_token=" + c.ApiKey
