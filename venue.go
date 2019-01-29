1c8722d70e50072677c2e2323db63cc5b3b2c0e0
Joe Sweeny <joe.sweeny@opia-sp.com>
2019-01-29 17:05:57 +0000

    Add venue retrieval support

---
+++ venue.go
@@ -0,0 +1,21 @@
+package sportmonks
+
+import "strconv"
+
+const venueSeasonUri = "/api/v2.0/venues/seasons"
+
+func (c *Client) BySeasonId(seasonId int) (*VenuesResponse, error) {
+	url := c.BaseURL + venueSeasonUri + strconv.Itoa(seasonId) + "?api_token=" + c.ApiKey
+
+	req, err := buildRequest("GET", url, nil, 0, []string{})
+
+	if err != nil {
+		return nil, err
+	}
+
+	r := new(VenuesResponse)
+
+	err = c.sendRequest(req, &r)
+
+	return r, err
+}
