3510676df317e37f1867f233bf2873c178f77fec
Joe Sweeny <joesweeny84@hotmail.com>
2019-10-03 12:19:50 +0100

    Add refactored client

--- http.go
+++ http.go
@@ -4,86 +4,54 @@ import (
 	"context"
 	"encoding/json"
 	"io"
-	"io/ioutil"
 	"net/http"
 	"strconv"
 	"strings"
-	"time"
 )
 
-const defaultBaseUri = "https://soccer.sportmonks.com"
-const defaultRetries = 5
-const defaultBackOffExponent = 2
-const defaultBackOffLimit = 120
-const defaultBackOffTotal = 1
+const defaultBaseUrl = "https://soccer.sportmonks.com/api/v2.0"
 
-// ApiClient is a HTTP request builder and sender
-type ApiClient struct {
-	client  *http.Client
-	baseURL string
-	apiKey  string
-	maxRetries int
-	backOffExponent int
-	backOffLimit int
-	backOffTotal int
+// Client is a HTTP request builder and sender
+type Client struct {
+	HTTPClient *http.Client
+	BaseURL string
+	Key  string
 }
 
-func (c *ApiClient) baseUrl() string {
-	if c.baseURL == "" {
-		c.baseURL = defaultBaseUri
+// NewClient creates a new Client with default settings. A key is required to
+// instantiate the Client
+func NewClient(key string) *Client {
+	return &Client{
+		HTTPClient: &http.Client{},
+		BaseURL: defaultBaseUrl,
+		Key: key,
 	}
-
-	return c.baseURL
 }
 
-func (c *ApiClient) httpClient(client *http.Client) *http.Client {
-	if c.client == nil {
-		c.client = http.DefaultClient
-	}
-
-	return c.client
-}
-
-func (c *ApiClient) retries() int {
-	if c.maxRetries == 0 {
-		c.maxRetries = defaultRetries
-	}
-
-	return c.maxRetries
-}
-
-func (c *ApiClient) createRequest(ctx context.Context, path string, includes []string) (*http.Request, error) {
+func (c *Client) createRequest(ctx context.Context, path string, includes []string) (*http.Request, error) {
 	return c.buildRequest(ctx, path, includes, 0)
 }
 
-func (c *ApiClient) createPaginatedRequest(ctx context.Context, path string, includes []string, page int) (*http.Request, error) {
+func (c *Client) createPaginatedRequest(ctx context.Context, path string, includes []string, page int) (*http.Request, error) {
 	return c.buildRequest(ctx, path, includes, page)
 }
 
-func (c *ApiClient) sendRequest(req *http.Request, response interface{}) error {
-	res, err := c.client.Do(req)
+func (c *Client) Do(req *http.Request, response interface{}) error {
+	resp, err := c.HTTPClient.Do(req)
 
 	if err != nil {
-		if c.maxRetries > 0 {
-			c.backOff()
-			c.maxRetries--
-			return c.sendRequest(req, response)
-		}
-
 		return err
 	}
 
-	if err := decodeResponseBody(res.Body, &response); err != nil {
+	if err := decodeResponseBody(resp.Body, &response); err != nil {
 		return err
 	}
-
-	// Reset Client here
 
 	return nil
 }
 
-func (c *ApiClient) buildRequest(ctx context.Context, path string, includes []string, page int) (*http.Request, error) {
-	url := c.baseURL + path + "?api_token=" + c.apiKey
+func (c *Client) buildRequest(ctx context.Context, path string, includes []string, page int) (*http.Request, error) {
+	url := c.BaseURL + path + "?api_token=" + c.Key
 
 	if page > 0 {
 		url = url + "&page=" + strconv.Itoa(page)
@@ -93,7 +61,7 @@ func (c *ApiClient) buildRequest(ctx context.Context, path string, includes []st
 		url = url + "&include=" + strings.Join(includes, ",")
 	}
 
-	req, err := http.NewRequest("GET", url, nil)
+	req, err := http.NewRequest(http.MethodGet, url, nil)
 
 	if err != nil {
 		return nil, err
@@ -107,24 +75,9 @@ func (c *ApiClient) buildRequest(ctx context.Context, path string, includes []st
 func decodeResponseBody(body io.ReadCloser, response interface{}) error {
 	defer body.Close()
 
-	b, err := ioutil.ReadAll(body)
-
-	if err != nil {
-		return nil
-	}
-
-	if err = json.Unmarshal([]byte(b), &response); err != nil {
+	if err := json.NewDecoder(body).Decode(response); err != nil {
 		return err
 	}
 
 	return nil
-}
-
-func (c *ApiClient) backOff() {
-	time.Sleep(time.Duration(c.backOffTotal))
-	c.backOffTotal = c.backOffTotal * c.backOffExponent
-
-	if c.backOffTotal > c.backOffLimit {
-		c.backOffTotal = c.backOffLimit
-	}
 }
