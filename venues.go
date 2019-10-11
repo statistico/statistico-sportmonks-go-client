f2683daee543ef0a809fa01592362e321db97f4b
Joe Sweeny <joesweeny84@hotmail.com>
2019-10-11 14:10:24 +0100

    Update venues

---
+++ venues.go
@@ -0,0 +1,57 @@
+package sportmonks
+
+import (
+	"context"
+	"fmt"
+	"net/url"
+)
+
+const venuesUri = "/venues"
+const venuesSeasonUri = "/venues/season"
+
+type Venue struct {
+	ID       int     `json:"id"`
+	Name     string  `json:"name"`
+	Surface  string  `json:"surface"`
+	Address  *string `json:"address"`
+	City     string  `json:"city"`
+	Capacity int     `json:"capacity"`
+	ImagePath    string  `json:"image_path"`
+	Coordinates *string `json:"coordinates"`
+}
+
+// VenueById returns a single Venue struct and supporting meta data.
+func (c *HTTPClient) VenueById(ctx context.Context, id int) (*Venue, *Meta, error) {
+	path := fmt.Sprintf(venuesUri + "/%d", id)
+
+	response := struct {
+		Data *Venue `json:"data"`
+		Meta *Meta    `json:"meta"`
+	}{}
+
+	err := c.getResource(ctx, path, url.Values{}, &response)
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return response.Data, response.Meta, err
+}
+
+// VenuesBySeasonId returns a slice of Venue struct and supporting meta data associated to a Season
+func (c *HTTPClient) VenuesBySeasonId(ctx context.Context, id int) ([]Venue, *Meta, error) {
+	path := fmt.Sprintf(venuesSeasonUri + "/%d", id)
+
+	response := struct {
+		Data []Venue `json:"data"`
+		Meta *Meta    `json:"meta"`
+	}{}
+
+	err := c.getResource(ctx, path, url.Values{}, &response)
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return response.Data, response.Meta, err
+}
---
+++ venues_test.go
@@ -0,0 +1,113 @@
+package sportmonks
+
+import (
+	"context"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+var venueResponse = `{
+	"data": {
+		"id": 200,
+		"name": "Turf Moor",
+		"surface": "grass",
+		"address": "Harry Potts Way",
+		"city": "Burnley",
+		"capacity": 22546,
+		"image_path": "https:\/\/cdn.sportmonks.com\/images\/soccer\/venues\/8\/200.png",
+		"coordinates": null
+	}
+}`
+
+var venueSeasonResponse = `{
+	"data": [
+		{
+			"id": 200,
+			"name": "Turf Moor",
+			"surface": "grass",
+			"address": "Harry Potts Way",
+			"city": "Burnley",
+			"capacity": 22546,
+			"image_path": "https:\/\/cdn.sportmonks.com\/images\/soccer\/venues\/8\/200.png",
+			"coordinates": null
+		}
+	]
+}`
+func TestVenueById(t *testing.T) {
+	t.Run("returns a single Venue struct", func(t *testing.T) {
+		url := defaultBaseUrl + "/venues/200?api_token=api-key"
+
+		server := mockResponseServer(t, venueResponse, 200, url)
+
+		client := newTestHTTPClient(server)
+
+		venue, _, err := client.VenueById(context.Background(), 200)
+
+		if err != nil {
+			t.Fatalf("Test failed, expected nil, got %s", err.Error())
+		}
+
+		assertVenue(t, venue)
+	})
+
+	t.Run("returns bad status code error", func(t *testing.T) {
+		url := defaultBaseUrl + "/venues/200?api_token=api-key"
+
+		server := mockResponseServer(t, errorResponse, 400, url)
+
+		client := newTestHTTPClient(server)
+
+		venue, _, err := client.VenueById(context.Background(), 200)
+
+		if venue != nil {
+			t.Fatalf("Test failed, expected nil, got %+v", venue)
+		}
+
+		assertError(t, err)
+	})
+}
+
+func TestVenueBySeasonId(t *testing.T) {
+	t.Run("returns a slice of Venue struct", func(t *testing.T) {
+		url := defaultBaseUrl + "/venues/season/12962?api_token=api-key"
+
+		server := mockResponseServer(t, venueSeasonResponse, 200, url)
+
+		client := newTestHTTPClient(server)
+
+		venues, _, err := client.VenuesBySeasonId(context.Background(), 12962)
+
+		if err != nil {
+			t.Fatalf("Test failed, expected nil, got %s", err.Error())
+		}
+
+		assertVenue(t, &venues[0])
+	})
+
+	t.Run("returns bad status code error", func(t *testing.T) {
+		url := defaultBaseUrl + "/venues/season/12962?api_token=api-key"
+
+		server := mockResponseServer(t, errorResponse, 400, url)
+
+		client := newTestHTTPClient(server)
+
+		venues, _, err := client.VenuesBySeasonId(context.Background(), 12962)
+
+		if venues != nil {
+			t.Fatalf("Test failed, expected nil, got %+v", venues)
+		}
+
+		assertError(t, err)
+	})
+}
+
+func assertVenue(t *testing.T, venue *Venue) {
+	assert.Equal(t,200, venue.ID)
+	assert.Equal(t,"Turf Moor", venue.Name)
+	assert.Equal(t,"grass", venue.Surface)
+	assert.Equal(t,"Harry Potts Way", *venue.Address)
+	assert.Equal(t,"Burnley", venue.City)
+	assert.Equal(t,22546, venue.Capacity)
+	assert.Equal(t,"https://cdn.sportmonks.com/images/soccer/venues/8/200.png", venue.ImagePath)
+	assert.Nil(t, venue.Coordinates)
+}
