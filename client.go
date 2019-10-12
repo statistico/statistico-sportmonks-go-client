149cd1deadb68ddce1aa451b2c1d42e1a6bb8e99
Joe Sweeny <joesweeny84@hotmail.com>
2019-10-12 12:13:25 +0100

    Add includes

--- client.go
+++ client.go
@@ -9,6 +9,12 @@ type Client interface {
 	// BookmakerById sends a request and returns a single Bookmaker resource.
 	BookmakerById(ctx context.Context, id int) (*Bookmaker, *Meta, error)
 
+	// CoachById returns a single Coach resource.
+	CoachById(ctx context.Context, id int) (*Coach, *Meta, error)
+
+	// CommentariesByFixtureId returns a slice of Commentary associated to a fixture
+	CommentariesByFixtureId(ctx context.Context, fixtureId int) ([]Commentary, *Meta, error)
+
 	// Continents sends a request and returns a slice of Continent struct and supporting meta data. The endpoint
 	// used within this method is paginated, to select the required page use the 'page' method argument. Page
 	// information including current page and total page are included within the Meta response.
@@ -20,5 +26,17 @@ type Client interface {
 
 	// Use the includes string slice to enrich the response data.
 	ContinentById(ctx context.Context, id int, includes []string) (*Continent, *Meta, error)
+
+	// Countries returns a slice of Country struct and supporting meta data. The endpoint used within this method
+	// is paginated, to select the required page use the 'page' method argument. Page information including current page
+	// and total page are included within the Meta response.
+
+	// Use the includes slice of string to enrich the response data.
+	Countries(ctx context.Context, page int, includes []string) ([]Country, *Meta, error)
+
+	// CountryById sends a request and returns a single Country struct.
+
+	// Use the includes slice of string to enrich the response data.
+	CountryById(ctx context.Context, id int, includes []string) (*Country, *Meta, error)
 }
 
