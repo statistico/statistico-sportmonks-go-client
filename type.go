44cfd25cdeec03034fdf29ff2b2d1c41b234a3fb
Joe Sweeny <joesweeny84@hotmail.com>
2024-09-24 22:40:18 +0100

    Add details to lineup player

--- type.go
+++ type.go
@@ -230,17 +230,29 @@ type (
 
 	// LineupPlayer provides information for a player of a team for a fixture.
 	LineupPlayer struct {
-		ID                int     `json:"id"`
-		SportID           int     `json:"sport_id"`
-		FixtureID         int     `json:"fixture_id"`
-		PlayerID          int     `json:"player_id"`
-		TeamID            int     `json:"team_id"`
-		PositionID        int     `json:"position_id"`
-		FormationField    *string `json:"formation_field"`
-		TypeID            int     `json:"type_id"`
-		FormationPosition int     `json:"formation_position"`
-		PlayerName        string  `json:"player_name"`
-		JerseyNumber      int     `json:"jersey_number"`
+		ID                int           `json:"id"`
+		SportID           int           `json:"sport_id"`
+		FixtureID         int           `json:"fixture_id"`
+		PlayerID          int           `json:"player_id"`
+		TeamID            int           `json:"team_id"`
+		PositionID        int           `json:"position_id"`
+		FormationField    *string       `json:"formation_field"`
+		TypeID            int           `json:"type_id"`
+		FormationPosition int           `json:"formation_position"`
+		PlayerName        string        `json:"player_name"`
+		JerseyNumber      int           `json:"jersey_number"`
+		Details           *LineupDetail `json:"details,omitempty"`
+	}
+
+	LineupDetail struct {
+		ID        int       `json:"id"`
+		FixtureID int       `json:"fixture_id"`
+		PlayerID  int       `json:"player_id"`
+		TeamID    int       `json:"team_id"`
+		LineupID  int       `json:"lineup_id"`
+		TypeID    int       `json:"type_id"`
+		Data      StatData  `json:"data"`
+		Type      *StatType `json:"type,omitempty"`
 	}
 
 	// LiveStandings provides league standing information for a team.
